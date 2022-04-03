b4df9c74b1e39dc3554ea43f192b03ff5c5c11fe
Pigniczki Mercédesz Tamara <h880036@stud.u-szeged.hu>
2022-04-03 12:47:12 +0200

    navbar

--- WeddingShop/src/app/app-routing.module.ts
+++ WeddingShop/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DummyComponent } from './dummy/dummy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",component: DummyComponent},
+  {path:"shop",component: DummyComponent},
+  {path:"signup",component: DummyComponent},
+  {path:"shoppingbag",component: DummyComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- WeddingShop/src/app/app.module.ts
+++ WeddingShop/src/app/app.module.ts
@@ -4,17 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { DummyComponent } from './dummy/dummy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DummyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
