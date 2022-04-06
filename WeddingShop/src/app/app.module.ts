4777dc02553792b90b5b031dd7848a5ae7ae4216
Pigniczki Mercédesz Tamara <h880036@stud.u-szeged.hu>
2022-04-06 19:23:24 +0200

    REgsiztráció

--- WeddingShop/src/app/app.module.ts
+++ WeddingShop/src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,6 +20,7 @@ import { ShopComponent } from './pages/shop/shop.component';
 import { ShoppingbagComponent } from './pages/shoppingbag/shoppingbag.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { FooterComponent } from './footer/footer.component';
+import { FirebaseService } from './services/firebase.service';
 
 
 
@@ -34,9 +41,15 @@ import { FooterComponent } from './footer/footer.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    AngularFireModule.initializeApp({apiKey: "AIzaSyCu0uqzyieykHN8ZG9jDKlRyvZCTMXTHcI",
+                                    authDomain: "fir-angular-cbe7c.firebaseapp.com",
+                                    projectId: "fir-angular-cbe7c",
+                                    storageBucket: "fir-angular-cbe7c.appspot.com",
+                                    messagingSenderId: "993850649502",
+                                    appId: "1:993850649502:web:e5e89fe492d30723b9c48f"})
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- WeddingShop/src/app/pages/home/home.component.ts
+++ WeddingShop/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FirebaseService } from '../../services/firebase.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  @Output() isLogout = new EventEmitter<void>()
+  constructor(public firebaseService: FirebaseService) { }
 
   ngOnInit(): void {
+  }
+  logout(){
+    this.firebaseService.logout()
+    this.isLogout.emit()
   }
 
 }
