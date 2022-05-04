171cac31267beedb0090f3810e0b398f6a611237
Pigniczki Mercédesz Tamara <h880036@stud.u-szeged.hu>
2022-05-04 11:03:35 +0200

    logout error

--- WeddingShop/src/app/pages/signup/signup.component.ts
+++ WeddingShop/src/app/pages/signup/signup.component.ts
@@ -24,25 +24,74 @@ export class SignupComponent implements OnInit {
   constructor(public firebaseService : FirebaseService, private router: Router) { }
 
   ngOnInit(): void {
-    if(localStorage.getItem('user')!== null)
+    if(sessionStorage.getItem('user')!== null)
     this.isSignedIn= true
     else
     this.isSignedIn = false
   }
+
   async onSignup(email:string,password:string){
     await this.firebaseService.signup(email,password)
+    .then(() => {
+      this.isSignedIn = true
+      this.users.push({email: email, password: password});
+      sessionStorage.setItem('token',JSON.stringify(this.users));
+      this.router.navigate(["/home"]);
+    }).catch((error) => {
+      switch (error.code) {
+        case "auth/email-already-exists":
+        {
+            alert("E-mail cím már foglalt!");
+            break;
+        }
+        case "auth/invalid-password":
+        {
+           alert("Nem megfelelő jelszó: legalább 6 karakter.");
+           break;
+        }
+           default:
+        {
+            alert("Email cím már foglalt, vagy nem megfelelő jelszó!");
+            break;
+        }
+      }
+    });
+
+   /* await this.firebaseService.signup(email,password)
     if(this.firebaseService.isLoggedIn)
     this.isSignedIn = true
     this.users.push({email: email, password: password});
     sessionStorage.setItem('token',JSON.stringify(this.users));
-    this.router.navigate(["/home"]);
+    this.router.navigate(["/home"]);*/
   }
   async onSignin(email:string,password:string){
-    await this.firebaseService.signin(email,password)
+    await this.firebaseService.signin(email, password)
+    .then(() => {
+              this.isSignedIn = true
+              this.users.push({email: email, password: password});
+              sessionStorage.setItem('token',JSON.stringify(this.users));
+              this.router.navigate(["/home"]);
+    }).catch((error) => {
+      switch (error.code) {
+        case "auth/invalid-email":
+        case "auth/wrong-password":
+        case "auth/user-not-found":
+        {
+           alert("Nem megfelelő jelszó vagy e-mail");
+           break;
+        }
+           default:
+        {
+            alert("Váratlan hiba törlént!");
+            break;
+        }
+      }
+    });
+  /*  await this.firebaseService.signin(email,password)
     if(this.firebaseService.isLoggedIn)
     this.isSignedIn = true
     this.users.push({email: email, password: password});
     sessionStorage.setItem('token',JSON.stringify(this.users));
-    this.router.navigate(["/home"]);
+    this.router.navigate(["/home"]);*/
   }
 }
